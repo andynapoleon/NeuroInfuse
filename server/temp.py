718566cbc8f8f554257e18253aab4d43e0dddd1c
xuanvietchu <vietxuanchu657@gmail.com>
2025-02-16 00:52:04 -0700

    half precision, memory efficient, inference_mode. 30s infer time

---
+++ server/temp.py
@@ -0,0 +1,11 @@
+# import torch
+# print("PyTorch version:", torch.__version__)
+# print("CUDA available:", torch.cuda.is_available())
+
+import cv2
+print(cv2.__version__)
+
+
+# python scripts/inference.py --outdir results --testdir examples --num_samples 1 --sample_steps 10
+# myenv\Scripts\activate
+# pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 -f https://download.pytorch.org/whl/torch_stable.html
